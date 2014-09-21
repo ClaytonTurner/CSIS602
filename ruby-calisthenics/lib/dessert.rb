651796eb001b601e4f7246b36a329e642b94392f
Learn2Mine Team <learn2mine@gmail.com>
2014-09-21 11:27:45 -0400

    Added dessert class (see README for details)

--- ruby-calisthenics/lib/dessert.rb
+++ ruby-calisthenics/lib/dessert.rb
@@ -1,17 +1,27 @@
 class Dessert
+  attr_accessor :name
+  attr_accessor :calories
+
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    return true if @calories < 200
+    return false
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
   def initialize(flavor)
-    # your code here
+    @calories = 5
+    @name = flavor + " jelly bean"
+  end
+  def delicious?
+    return true unless @flavor=="licorice"
   end
 end
