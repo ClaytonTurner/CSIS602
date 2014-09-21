b19f1f5b4578be27964adc69c671e25a93844f2b
Learn2Mine Team <learn2mine@gmail.com>
2014-09-21 11:47:50 -0400

    Wrote winner and tournament methods for rock paper scissors

--- ruby-calisthenics/lib/rock_paper_scissors.rb
+++ ruby-calisthenics/lib/rock_paper_scissors.rb
@@ -1,14 +1,43 @@
 class RockPaperScissors
-
+  VALID=['R','P','S']
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    raise NoSuchStrategyError unless VALID.include?(player1[1]),"Strategy must be one of R,P,S"
+    raise NoSuchStrategyError unless VALID.include?(player2[1]),"Strategy must be one of R,P,S"
+    if player1[1] == player2[1]
+	return player1
+    elsif player1[1] == 'R'
+	if player2[1] == 'P'
+		return player2
+	else
+		return player1
+	end
+    elsif player1[1] == 'P'
+	if player2[1] == 'R'
+		return player1
+	else
+		return player2
+	end
+    elsif player1[1] == 'S'
+	if player2[1] == 'P'
+		return player1
+	else
+		return player2
+	end
+    else
+	raise "Should be Unreachable "
+    end
+
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    #base case - 1 match
+    if tournament[0][0].is_a?(String) #Checking for a name
+	return self.winner(tournament)
+    end
+    return self.winner([self.tournament_winner(tournament[0]),self.tournament_winner(tournament[1])])
   end
 
 end
