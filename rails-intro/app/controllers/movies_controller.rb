7f4ee2797427238ea88a4364444c1f1de0fe4526
Clayton Turner <caturner3@g.cofc.edu>
2014-10-09 15:34:15 -0300

    RESTful preserved. Sort and filter work together

--- rails-intro/app/controllers/movies_controller.rb
+++ rails-intro/app/controllers/movies_controller.rb
@@ -8,45 +8,65 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    self.redirector # Preserves RESTfulness
+
     # Sort Behavior
-    self.sort_trigger
+# self.sort_trigger
 
     # Ratings Behavior
     @all_ratings = Movie.all_ratings
     @checked_ratings = @all_ratings
     self.rating_trigger
+    self.sort_trigger
   end
 
   def sort_trigger
-    if params[:sort].nil?
-      if session[:sort].nil?
-      else
-        @sort = session[:sort]
-        redirect_to movies_path(:sort => session[:sort])
-      end
-    else
-      @sort = params[:sort]
-      session[:sort] = params[:sort]
-      @movies = Movie.order(@sort.to_sym)
+    # sort has a value, use it
+    if params[:sort].present?
+        @sort = params[:sort]
+        session[:sort] = params[:sort]
+        @movies = Movie.order(@sort.to_sym)
     end
   end
 
   def rating_trigger
-    if params[:ratings].nil?
-      if session[:ratings].nil?
-      else
-        #session has a value - use it
-        redirect_to movies_path(:ratings => session[:ratings])
-      end
-    else
-      # params has a value - use it
+    # params has a value - use it
+    if params[:ratings].present?
       ratings_hash = params[:ratings]
       ratings_keys = ratings_hash.keys
       session[:ratings] = params[:ratings]
       @movies = Movie.find_all_by_rating(ratings_keys)
       @checked_ratings = ratings_keys
     end
-    
+  end
+
+  def redirector
+    psort = true if params[:sort].present?
+    pratings = true if params[:ratings].present?
+    ssort = true if session[:sort].present?
+    sratings = true if session[:ratings].present?
+    ratings_switch = nil
+    sort_switch = nil
+    redirect = false
+    if psort && pratings # both true, do nothing
+    elsif psort # psort is true, pratings is false
+      sort_switch = params[:sort]
+      if sratings # psort && sratings
+        ratings_switch = session[:ratings]
+        redirect = true
+      end
+    elsif pratings # pratings is true, psort is false
+      ratings_switch = params[:ratings]
+      if ssort # pratings && ssort
+        sort_switch = session[:sort]
+        redirect = true
+      end
+    else # no params, let's check session
+      sort_switch = session[:sort] if ssort
+      ratings_switch = session[:ratings] if sratings
+      redirect = true if sort_switch || ratings_switch
+    end
+    redirect_to movies_path(:ratings => ratings_switch,:sort => sort_switch) if redirect
   end
 
   def new
